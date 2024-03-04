400ae164960ed4d3cabcc08340c1906a1f7bd059
Zoltán Dul <zoltan.dul@gmail.com>
2024-03-04 08:05:28 +0100

    Separaring log file

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
-// Conserved Core GO_TOOL - Main PHP File
-// Get visitor's IP address. Considering the possibility of the visitor being behind a proxy
-$ip = $_SERVER['REMOTE_ADDR'];
-if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
-    $ip = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
-}
-
-// Get visitor's browser information
-$browser = $_SERVER['HTTP_USER_AGENT'];
-
-// Create a string to log
-$logString = date("Y-m-d H:i:s") . " - IP: " . $ip . " - Browser: " . $browser . "\n";
-
-// Specify the file path
-$filePath = "logs/visitors.log";
-
-// Append the logString to the file
-file_put_contents($filePath, $logString, FILE_APPEND);
-
+// // Conserved Core GO_TOOL - MAIN FILE
+// Include the log.php file
+include("log.php"); 
 ?>
 <!DOCTYPE html>
 <html lang="en">
